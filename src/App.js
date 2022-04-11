06081e14fec4f305bbe02cb398ce070c50e4af48
GeorgeCheng1023 <ck40292@gmail.com>
2022-04-11 16:25:13 +0800

    del unneccerily

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import logo from "./logo.svg";
 import "./App.css";
-
+import Expenses from "./components/Expenses";
+const expenses = [
+  { name: "Apple", price: 400, date: new Date(2022, 3, 4) },
+  { name: "Banana", price: 300, date: new Date(2022, 4, 11) },
+  { name: "Lemon", price: 150, date: new Date(2022, 4, 5) },
+  { name: "Straw Berry", price: 250, date: new Date(2022, 3, 29) },
+];
 function App() {
   return (
     <div>
       <h1>Hello React!</h1>
+      <Expenses expenses={expenses} />
     </div>
   );
 }
